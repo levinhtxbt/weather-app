edd4c231546fd269eaceae4eff0f01f6aa8bfb01
Lê Vinh <levinhtxbt@gmail.com>
2023-04-23 16:24:32 +0000

    add request transform

--- Gateway/Program.cs
+++ Gateway/Program.cs
@@ -1,3 +1,5 @@
+using Yarp.ReverseProxy.Transforms;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddYamlFile("appsettings.yml", optional: false, reloadOnChange: true);
 // Add services to the container.
@@ -7,7 +9,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .AddTransforms(builderContext =>
+    {
+        builderContext.AddRequestTransform(transform =>
+        {
+            transform.ProxyRequest.Headers.Add("X-CorrelationId", new Guid().ToString("N"));
+            return new ValueTask();
+        });
+        
+    });
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
